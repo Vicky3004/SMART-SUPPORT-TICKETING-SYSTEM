87da1d59f3f2c195466ba501d2a80bf30d660540
vicky3004 <vignesh.org.in@gmail.com>
2026-05-05 15:08:05 +0530

    Changes Doned

--- BACKEND/src/main/java/com/example/support/SupportApplication.java
+++ BACKEND/src/main/java/com/example/support/SupportApplication.java
@@ -1,7 +1,6 @@
 package com.example.support;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SupportApplication {
     public static void main(String[] args) {
